7cfa9aa31dcbdaf1b1216ec8b4889ccbc8b832f8
Carly Richardson <63616608+carlyannrich@users.noreply.github.com>
2020-12-11 00:57:03 +0000

    added html/css to the logic of game - still more to do

--- The Odin Project/rockPaperScissorsGame/script.js
+++ The Odin Project/rockPaperScissorsGame/script.js
@@ -1,4 +1,17 @@
 // establishes computer random choice
+let amountOfGame = 0;
+let playerTotal = 0;
+let compTotal = 0;
+
+const buttons = document.querySelectorAll(".btn");
+
+const setGameTotals = (id, player, total) => {
+  const element = document.getElementById(id);
+  element.innerHTML = `${player} score: ${total}`;
+};
+
+// plays one round of the game - works out who won
+
 function computerPlay() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -10,57 +23,21 @@ const outcome = {
   draw: ["rockrock", "paperpaper", "scissorsscissors"],
 };
 
-let playerTotal = 0;
-let compTotal = 0;
-
-// plays one round of the game - works out who won
-
-function singleRound() {
-  // assigns computer and player selection
-  const playerSelection = prompt(
-    "Enter your choice.. either paper, rock or scissors:"
-  ).toLowerCase();
-  console.log("You have chosen " + playerSelection);
+function singleRound(playersChoice) {
   const computerSelection = computerPlay();
-  console.log("Computer has chosen " + computerSelection);
 
-  // switch (playerSelection + computerSelection) {
-  //   case "rockscissors":
-  //   case "paperrock":
-  //   case "scissorspaper":
-  //     return "Hooray! You Win!";
-  //   case "rockpaper":
-  //   case "paperscissors":
-  //   case "scissorsrock":
-  //     return "Oh dear... You Lose!";
-  //   case "rockrock":
-  //   case "paperpaper":
-  //   case "scissorsscissors":
-  //     return "It's a Draw!";
-  // }
-
-  if (outcome.win.includes(playerSelection + computerSelection)) {
-    playerTotal += 1;
+  if (outcome.win.includes(playersChoice + computerSelection)) {
+    playerTotal++;
     return "Hooray! You Win!";
-  } else if (outcome.lose.includes(playerSelection + computerSelection)) {
-    compTotal += 1;
+  } else if (outcome.lose.includes(playersChoice + computerSelection)) {
+    compTotal++;
     return "Oh dear... You Lose!";
-  } else if (outcome.draw.includes(playerSelection + computerSelection)) {
+  } else if (outcome.draw.includes(playersChoice + computerSelection)) {
     return "It's a Draw!";
   } else {
     return "Uhoh error...";
   }
 }
-
-function game() {
-  let i = 1;
-  while (i <= 5) {
-    console.log(singleRound());
-    i++;
-  }
-}
-
-game();
 
 function totalScore() {
   if (playerTotal > compTotal) {
@@ -72,4 +49,28 @@ function totalScore() {
   }
 }
 
-totalScore();
+function game(playersChoice) {
+  if (amountOfGame === 0)
+    document.getElementById("output").innerHTML = "good luck...";
+  if (amountOfGame < 4) {
+    singleRound(playersChoice);
+    setGameTotals("playerTotal", "player", playerTotal);
+    setGameTotals("compTotal", "computer", compTotal);
+    amountOfGame++;
+  } else {
+    // game over func
+    // highlight winner
+    // reset amount of games
+    document.getElementById("output").innerHTML = totalScore();
+    amountOfGame = 0;
+    playerTotal = 0;
+    compTotal = 0;
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const playersChoice = event.target.id;
+    game(playersChoice);
+  });
+});

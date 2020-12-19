d989249d68e2e64ca307fc8de915712ca64b7a16
Carly Richardson <63616608+carlyannrich@users.noreply.github.com>
2020-12-19 23:24:23 +0000

    day 23 commit - more The Odin Project javascript exercises

---
+++ js_exercisesTOP/leapYears.js
@@ -0,0 +1,7 @@
+// const leapYears = (year) => {
+//   if (year % 4 === 0 && year % 100 !== 0 && year % 400 === 0) return true;
+//   else return false;
+// };
+
+// leapYears(2000) // is a leap year: returns true
+// leapYears(1985) // is not a leap year: returns false
